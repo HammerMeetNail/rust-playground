7e06368332f9389ce725ff6cf56aa36670b6b6b9
Dave O'Connor <1656866+HammerMeetNail@users.noreply.github.com>
2020-07-05 12:49:25 -0400

    Add unit test

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -22,7 +22,36 @@ impl Config {
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.filename)?;
     
-    println!("With text:\n{}", contents);
+    search(&config.query, &contents);
 
     return Ok(())
+}
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.contains(query) {
+            println!("{}", line);
+            results.push(line);
+        }
+    }
+
+    return results
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
 }
